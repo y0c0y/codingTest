89dd5d46abb025343e9680800fc69aa09f5d584d
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2023-10-14 21:05:48 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 1756 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,25 +1,23 @@
 import math
 
-def PrimeNumbers(n):
-    arr = [True for i in range(n + 1)]  # 0,1을 제외한 모든 숫자가 소수(True)인 것으로 설정하고 시작한다.
+def PrimeNumbers():
+    cnt = 10000
+    arr = [True for i in range(cnt+1)]  # 0,1을 제외한 모든 숫자가 소수(True)인 것으로 설정하고 시작한다.
 
     # 에라토스테네스의 체 알고리즘
-    for i in range(2, int(math.sqrt(n)) + 1):
+    for i in range(2, int(math.sqrt(cnt)) + 1):
         if arr[i] == True:  # i가 소수인 경우
             # i를 제외한 i의 모든 배수를 지우기
             j = 2
-        while i * j <= n:
+        while i * j <= cnt:
             arr[i * j] = False
             j += 1
     return arr
 
-
-
 T = int(input())
-
+arr = PrimeNumbers()
 for i in range(T):
     n = int(input())  # 2부터 1000까지 모든 수에 대하여 소수를 찾을 것이다.
-    arr = PrimeNumbers(n)
     a1 = 0
     a2= 0
     minGap = float("inf")
