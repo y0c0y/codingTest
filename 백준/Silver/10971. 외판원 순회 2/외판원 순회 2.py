693f503088bce62123e8421c3c01b1ccae9c4c0a
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2023-10-15 21:29:02 +0900

    [Silver II] Title: 외판원 순회 2, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,31 +1,29 @@
 import sys
 
-
 def dfs(start, now, value, cnt):
     global ans
-    if cnt == N:
+    if cnt == n:
         if a[now][start]:
             value += a[now][start]
             if ans > value:
                 ans = value
         return
-
     if value > ans:
         return
-
-    for i in range(N):
+    for i in range(n):
         if not visited[i] and a[now][i]:
-            visited[i] = 1
-            dfs(start, i, value + a[now][i], cnt + 1)
-            visited[i] = 0
+            visited[i] = True
+            dfs(start, i , value+a[now][i], cnt+1)
+            visited[i] = False
 
-
-N = int(input())
-a = [list(map(int, input().split()))for _ in range(N)]
+n = int(sys.stdin.readline())
+a = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 ans = sys.maxsize
-visited = [0] * N
-for i in range(N):
-    visited[i] = 1
-    dfs(i, i, 0, 1)
-    visited[i] = 0
+visited = [False]*n
+for i in range(n):
+    visited[i] = True
+    dfs(i,i,0,1)
+    visited[i] =False
 print(ans)
+
+
