b3434a66ff6589d859278a16d0ecf67ecb90356b
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2023-10-25 22:44:28 +0900

    [Gold III] Title: 아침 산책, Time: 872 ms, Memory: 97220 KB, Score: 3 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,40 +1,48 @@
-from sys import setrecursionlimit, stdin
-setrecursionlimit(10**9)
-"""문제 풀이 논리
-1. 실외 점을 기준으로 인접해있는 실내 노드 개수를 count한다.
-2. 실외 점을 중간에 놓고 실내 점 n개가 붙어있을 때 갈 수 있는 경로의 수는 n * 1(중간 실외 점 선택) * (n-1) = n*(n-1)에 해당.
-3. 실외 노드끼리 연결되는 경우는 1) 실외끼리 인접 노드로 연결될 때 2) 중간에 실내 노드를 끼고 연결할 때. 이를 분리해서 생각.
-"""
-def dfs(v, cnt): # v: 정점 번호 & cnt: 실외와 연결된 실내 노드 개수 카운트
-    
-    visited[v] = True
-    
-    for i in graph[v]: # 노드 v와 연결된 인접 노드를 하나씩 불러온다.
-        if location[i] == 1:  # 해당 노드의 위치가 실내이면
-            cnt += 1 # 실내 개수 카운트에 +1을 해준다
-        elif not visited[i] and location[i] == 0: # 방문하지 않고 해당 i 점의 위치가 실외이면
-            cnt = dfs(i, cnt) # 해당 실외 점을 기준으로 dfs를 돈다!
-    
-    return cnt
-ans = 0
-n = int(stdin.readline()) # 정점 수 받기
+import sys
 
-location = [0]+list(map(int, stdin.readline().strip())) # location 정보 받아오기: 앞에 0 추가하는 이유는 노드의 인덱스 번호를 1부터 시작하기 위해
 
-graph = [[] for _ in range(n+1)] # 1번 노드부터 n번 노드까지 다 받아와야 하니
+n = int(sys.stdin.readline())
+inNOut = list(map(int, sys.stdin.readline().strip()))
 
-for _ in range(n-1): # 셋째 줄부터 n+1번 줄까지 받기
-    a, b = map(int, stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    if location[a] == 1 and location[b] == 1: # 둘다 실내이면
-        ans +=2 # 서로 방문하는 케이스가 2가지이니 이걸 정답에 함께 바로 세는 걸로.
-        
-sum = 0
-visited = [False] * (n+1)
-for i in range(1, n+1):
-    if not visited[i] and location[i] == 0: # 실외인 애들을 기준으로
-        x = dfs(i, 0) # 현재 cnt = 0
-        sum += x*(x-1)  #실외인 노드를 기준으로 인접 노드 애들 개수 세는 게 총 n*(n-1)이니 실외 노드 걸릴 때마다 이걸 전부 세기
 
-print(sum + ans)
+def setVisitedFalse():
+    return [False for _ in range(n + 1)]
+
+
+graph = {}
+for i in range(n):
+    graph[i + 1] = [[], inNOut[i]]
+
+totalWay = []
+
+
+def dfs(start, visited):
+    visited[start] = True
+    for i in graph[start][0]:
+        if not visited[i]:
+            for j in graph[i][0]:
+                # print(f"in dfs {j}")
+                if graph[j][1]:
+                    # print(visited)
+                    totalWay.append(visited)
+                    return
+                visited[j] = True
+                dfs(i, visited)
+    return
+
+
+for _ in range(n - 1):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u][0].append(v)
+    graph[v][0].append(u)
+
+# print(graph)
+
+for i in range(n):
+    if inNOut[i]:
+        # print(f"i =={i} and {graph[i + 1][0]}")
+        for j in graph[i + 1][0]:
+            # print(j)
+            visited = dfs(j, setVisitedFalse())
+            # print(visited)
+print(len(totalWay))
