1c7094354658888466402df23d3294633c20ef05
Cho Yoon-Hee <68114427+y0c0y@users.noreply.github.com>
2024-01-12 14:19:37 +0900

    [Bronze I] Title: 소인수분해, Time: 1176 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,12 +1,9 @@
 # 백준 11653
 
 n = int(input())
-i = 2
 
-while n != 1:
-    if n % i == 0:
-        print(i)
-        n //= i
-    else:
-        i += 1
-
+if n != 1:
+    for i in range(2, n + 1):
+        while n % i == 0:
+            print(i)
+            n = n // i
