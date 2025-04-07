91216e971e10816ddbaad3dd326a67a671fe9363
조윤희 <68114427+y0c0y@users.noreply.github.com>
2025-04-08 05:49:32 +0900

    [Bronze III] Title: ACM 호텔, Time: 56 ms, Memory: 5776 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.cs
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cs
@@ -1,28 +1,20 @@
- int n = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= n; i++)
-        {
-            string[] input = Console.ReadLine().Split(' ');
-            
-            int H = int.Parse(input[0]);
-            int W = int.Parse(input[1]);
-            int N = int.Parse(input[2]);
-
-            int x = 1;
-            int y = 1;
-            while(true)
+            for (int i = 1; i <= n; i++)
             {
-                N--;
-                if(N == 0) break;
-                if(y == H)
-                {
-                    y = 1;
-                    x++;
-                }
-                else{
-                    y++;
-                }
+                string[] input = Console.ReadLine().Split(' ');
                 
-            }
-            Console.WriteLine(y*100+x);
-        }
+                int H = int.Parse(input[0]);
+                int W = int.Parse(input[1]);
+                int N = int.Parse(input[2]);
+    
+                int X = N / H;
+                int Y = N % H;
+                if (Y == 0) 
+                    {Y = H;}
+                else {X++;}
+    
+                int room = Y * 100 + X;
+                
+                Console.WriteLine(room);
+             }
